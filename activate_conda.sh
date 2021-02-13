eaa31d8553b1b0c82a4a880baa54ded5f456a62d
Hayden Bronson <hbronson@bu.edu>
2021-02-13 12:40:09 -0800

    changing our code to c7 :)

---
+++ activate_conda.sh
@@ -0,0 +1,8 @@
+export TWHOME=$PWD
+export PYTHONPATH=${PYTHONPATH}:$PWD:$PWD/coffea/
+
+conda activate coffeatest
+
+#conda activate coffeadev
+
+#( conda activate daskanalysisenv && jupyter notebook --no-browser )

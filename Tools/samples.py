719ebe0d783b616ca7455cc0ea83eb31113abedc
Hayden Bronson <hbronson@bu.edu>
2021-03-15 09:00:18 -0700

    trilep_analysis working

--- Tools/samples.py
+++ Tools/samples.py
@@ -14,6 +14,9 @@ data_path = os.path.join(cfg['meta']['localSkim'], tag)
 groups_2018 = {
     'tW_scattering': ['/tW_scattering[-_]'],
     'topW_v2':       ['/ProjectMetis_TTWJetsToLNuEWK'],
+    'topW_v3':       ['/ProjectMetis_TTWplusJetsToLNuEWK', '/ProjectMetis_TTWminusJetsToLNuEWK'],
+    'topW_EFT_cp8':  ['/ProjectMetis_TTWJetsToLNuEWK_5f_EFT_myNLO_full'],
+    'topW_EFT_mix':  ['/ProjectMetis_TTWJetsToLNuEWK_5f_EFT_mix_myNLO_full'],
     # careful - TTX is a sum of all TTX but TTW
     'TTXnoW':        ['/TTZToLLNuNu[-_]', '/ST_tWll[-_]', '/ST_tWnunu[-_]', '/TH[W,Q][-_]', '/TT[T,W,Z][T,W,Z][-_]', '/tZq[-_]', '/ttHToNonbb[-_]'],
     'TTW':           ['/TTWJets'],
@@ -21,10 +24,13 @@ groups_2018 = {
     'TTZ':           ['/TTZToLLNuNu[-_]', '/ST_tWll[-_]', '/ST_tWnunu[-_]', '/tZq[-_]', '/TT[W,Z][W,Z][-_]'],
     'TTTT':          ['/TTTT[-_]'],
     'ttbar':         ['/TTTo2L2Nu', '/TTToSemiLeptonic', '/ST_[s,t]-channel', '/ST_tW[-_]'],
-    'ttbar1l':       ['/TTToSemiLeptonic', '/ST_[s,t]-channel', '/ST_tW[-_]'],
+    'top':           ['/TTTo2L2Nu', '/TTToSemiLeptonic', '/ST_[s,t]-channel', '/ST_tW[-_]'],
+    'top1l':         ['/TTToSemiLeptonic', '/ST_[s,t]-channel', '/ST_tW[-_]'],
+    'ttbar1l':       ['/TTToSemiLeptonic'],
     #'ttbar2l':       ['/TTTo2L2Nu', '/ST_[s,t]-channel', '/ST_tW[-_]'],
-    'ttbar2l':       ['/TTTo2L2Nu', '/ST_t-channel', '/ST_tW[-_]'],
-    'ttbar1l_MG':    ['/TTJets_SingleLept', '/ST_[s,t]-channel'],
+    'top2l':         ['/TTTo2L2Nu', '/ST_t-channel', '/ST_tW[-_]'],
+    'ttbar2l':       ['/TTTo2L2Nu'],
+    'ttbar1l_MG':    ['/TTJets_SingleLept'],
     'TTW':           ['/TTWJets'],
     'wjets':         ['/W[1-4]JetsToLNu[-_]'],
     'diboson':       ['/WZTo.*amcatnloFXFX', '/WWTo', '/ZZTo', '/[W,Z][W,Z][W,Z][-_]'], # there's also a powheg sample
@@ -32,7 +38,29 @@ groups_2018 = {
     'WW':            ['/WWTo'], 
     'WZ':            ['/WZTo.*amcatnloFXFX'], # there's also a powheg sample
     'DY':            ['/DYJetsToLL'],
-    'MuonEG':        ['/MuonEG_Run2018[ABCD]'],
+
+    'TTWZ':             ['/TTWZ[-_]'],
+    'TTZToLLNuNu_M-10': ['/TTZToLLNuNu_M-10[-_]'],
+    'TTZToLL_M-1to10':  ['/TTZToLL_M-1to10[-_]'],
+    'TTZToQQ':          ['/TTZToQQ[-_]'],
+    'ST_tWll':          ['/ST_tWll[-_]'],
+    'tZq':              ['/tZq[-_]'],
+
+    'ttHToNonbb':       ['/ttHToNonbb[-_]'],
+    'THW':              ['/THW[-_]'],
+    'THQ':              ['/THQ[-_]'],
+
+    'TTWJetsToLNu':     ['/TTWJetsToLNu'],
+    'TTWJetsToQQ':      ['/TTWJetsToQQ'],
+
+    'TTToSemiLeptonic': ['/TTToSemiLeptonic'],
+    'ST_t-channel':     ['/ST_t-channel'],
+    'ST_s-channel':     ['/ST_s-channel'],
+    'ST_tW':            ['/ST_tW[-_]'],
+
+    'MuonEG':           ['/MuonEG_Run2018[ABCD]'],
+    'EGamma':           ['/EGamma_Run2018[ABCD]'],
+    'DoubleMuon':       ['/DoubleMuon_Run2018[ABCD]'],
 }
 
 #samples_2016 = glob.glob(data_path_2016 + '/*')
--- Tools/triggers.py
+++ Tools/triggers.py
@@ -1,5 +1,6 @@
 import numpy as np
-import awkward as ak
+import awkward1 as ak
+import re
 ## happily borrowed from https://github.com/bu-cms/bucoffea/blob/master/bucoffea/helpers/helpers.py
 
 def mask_or(ev, collection, masks):
@@ -63,9 +64,9 @@ def getFilters(ev, year=2018, dataset='None'):
             "BadPFMuonFilter",
             "ecalBadCalibFilterV2"
         ]
-
+        
         filters_data = filters_MC + ["eeBadScFilter"]
-
+        
     elif year == 2017:
         filters_MC = [\
             "goodVertices",
@@ -76,7 +77,7 @@ def getFilters(ev, year=2018, dataset='None'):
             "BadPFMuonFilter",
             "ecalBadCalibFilterV2"
         ]
-
+        
         filters_data = filters_MC + ["eeBadScFilter"]
 
     elif year == 2016:
@@ -88,18 +89,22 @@ def getFilters(ev, year=2018, dataset='None'):
             "EcalDeadCellTriggerPrimitiveFilter",
             "BadPFMuonFilter",
         ]
-
+        
         filters_data = filters_MC + ["eeBadScFilter"]
-
+        
     if dataset.lower().count('muon') or dataset.lower().count('electron'):
         return mask_and(ev, "Flag", filters_data)
     else:
         return mask_and(ev, "Flag", filters_MC)
-
-def getTriggers(ev, year=2018, dataset='None'):
+        
+def getTriggers(ev, leading_pdg, subleading_pdg, year=2018, dataset='None'):
     # these are the MET triggers from the MT2 analysis
-
+    
     triggers = {}
+    
+    same_flavor = (abs(leading_pdg) == abs(subleading_pdg))
+    leading_ele = (abs(leading_pdg) == 11)
+    leading_mu  = (abs(leading_pdg) == 13)
 
     if year == 2018:
         triggers['MuonEG'] = [\
@@ -109,28 +114,62 @@ def getTriggers(ev, year=2018, dataset='None'):
             "Mu37_Ele27_CaloIdL_MW",
         ]
 
+        triggers['DoubleMuon'] = [\
+            "Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL",
+            "Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ",
+            "Mu37_TkMu27",
+        ]
+
+        triggers['DoubleEG'] = [\
+            "Ele23_Ele12_CaloIdL_TrackIdL_IsoVL",
+            "Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ",
+        ]
+
         triggers['MET'] = [\
             "PFMET120_PFMHT120_IDTight",
             "PFMET120_PFMHT120_IDTight_PFHT60",
             "PFMETNoMu120_PFMHTNoMu120_IDTight",
             "PFMETNoMu120_PFMHTNoMu120_IDTight_PFHT60",
         ]
-
+        
     elif year == 2017:
         triggers['MET'] = [\
             "PFMET120_PFMHT120_IDTight",
             "PFMETNoMu120_PFMHTNoMu120_IDTight",
         ]
-
+        
     elif year == 2016:
         triggers['MET'] = [\
             "PFMET120_PFMHT120_IDTight",
             "PFMETNoMu120_PFMHTNoMu120_IDTight",
         ]
+        
+    #print (dataset)
+    if re.search(re.compile("MuonEG"), dataset):
+        #print ("In MuonEG branch")
+        trigger = mask_or(ev, "HLT", triggers["MuonEG"])
+        #print (sum(trigger & ~same_flavor))
+        return (trigger & ~same_flavor)
+        #return trigger
 
-    if dataset.lower().count('muon') or dataset.lower().count('electron'):
-        return mask_or(ev, "HLT", triggers[dataset])
+    elif re.search(re.compile("DoubleMuon"), dataset):
+        #print ("In DoubleMuon branch")
+        trigger = mask_or(ev, "HLT", triggers["DoubleMuon"])
+        #print (sum(trigger & same_flavor & leading_mu))
+        return (trigger & same_flavor & leading_mu)
+        #return trigger
+
+    elif re.search(re.compile("DoubleEG|EGamma"), dataset):
+        #print ("In EGamma branch")
+        trigger = mask_or(ev, "HLT", triggers["DoubleEG"])
+        #print (sum(trigger & same_flavor & leading_ele))
+        return (trigger & same_flavor & leading_ele)
+
     else:
-        return mask_or(ev, "HLT", triggers['MuonEG']) # should be OR of all dilepton triggers
-
+        #print ("In MC branch")
+        # these triggers aren't fully efficient yet. check if we're missing something.
+        mm = (mask_or(ev, "HLT", triggers['DoubleMuon']) & same_flavor & leading_mu)
+        ee = (mask_or(ev, "HLT", triggers['DoubleEG']) & same_flavor & leading_ele)
+        em = (mask_or(ev, "HLT", triggers['MuonEG']) & ~same_flavor)
+        return (mm | ee | em)
 
